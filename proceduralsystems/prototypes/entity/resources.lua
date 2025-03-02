8ba28e9784361bb7e7fec6cb594e3eb948f155c2
Zach Kolansky <zacharykolansky@gmail.com>
2025-03-01 20:01:15 -0600

    1.0.0
    Finished resources, stablely loads to main menu. Now working on autoplace controls

--- proceduralsystems/prototypes/entity/resources.lua
+++ proceduralsystems/prototypes/entity/resources.lua
@@ -66,10 +66,32 @@ local function resource(resource_parameters, autoplace_parameters, graphics_para
     }
 end
 
-
+local function create_procedural_ore(input_name, input_tint)
+  return resource(
+    {
+      name = input_name,
+      order = "b",
+      map_color = input_tint,
+      mining_time = 1,
+      walking_sound = sounds.ore,
+      mining_visualisation_tint = input_tint, --rgb(211, 120, 120)  --Color for ore 
+      --factoriopedia_simulation = simulations.factoriopedia_calcite,
+    },
+    {
+      probability_expression = 0
+    },
+    {
+      icon_subfolder = "",
+      icon_image = "procedural-ore-1.png",
+      entity_subfolder = "procedural-ore",
+      entity_image_sheet = "procedural-ore.png",
+    }
+  )
+end
 
 data:extend(
 {
+    --petroleum geyser-p
     {
         type = "resource",
         name = "petroleum-geyser-p",
@@ -94,7 +116,7 @@ data:extend(
           {
             {
               type = "fluid",
-              name = "petroleum-gas,
+              name = "petroleum-gas",
               amount_min = 10,
               amount_max = 10,
               probability = 1
@@ -176,6 +198,7 @@ data:extend(
         map_color = {r = 228, g = 208, b = 32}, --rgb(228, 208, 32)
         map_grid = false
     },
+    --steam-geyser-p
     {
         type = "resource",
         name = "steam-geyser-p",
@@ -200,10 +223,11 @@ data:extend(
           {
             {
               type = "fluid",
-              name = "petroleum-gas,
+              name = "steam",
               amount_min = 70,
               amount_max = 70,
-              probability = 1
+              probability = 1,
+              temperature = 250,
             }
           }
         },
@@ -282,6 +306,7 @@ data:extend(
         map_color = {r = 180, g = 180, b = 180}, --rgb(180, 180, 180)
         map_grid = false
     },
+    --water-geyser-p
     {
         type = "resource",
         name = "water-geyser-p",
@@ -306,7 +331,7 @@ data:extend(
           {
             {
               type = "fluid",
-              name = "water,
+              name = "water",
               amount_min = 10,
               amount_max = 10,
               probability = 1
@@ -389,6 +414,7 @@ data:extend(
         map_grid = false
     },
 
+    --covellite
     resource(
         {
           name = "covellite",
@@ -403,12 +429,13 @@ data:extend(
           probability_expression = 0
         },
         {
-            icon_subfolder = "ores_malcom/covellite",
-            icon_image = "collellite.png",
-            entity_subfolder = "procedural-ore",
-            entity_image_sheet = "copper-ore-desat.png",
+          icon_subfolder = "ores_malcom/covellite",
+          icon_image = "covellite-1.png",
+          entity_subfolder = "procedural-ore",
+          entity_image_sheet = "copper-ore-desat.png",
         }
     ),
+    --malachite
     resource(
         {
           name = "malachite",
@@ -423,10 +450,254 @@ data:extend(
           probability_expression = 0
         },
         {
-            icon_subfolder = "ores_malcom/malachite",
-            icon_image = "malachite.png",
-            entity_subfolder = "procedural-ore",
-            entity_image_sheet = "copper-ore-desat.png",
+          icon_subfolder = "ores_malcom/malachite",
+          icon_image = "malachite-1.png",
+          entity_subfolder = "procedural-ore",
+          entity_image_sheet = "copper-ore-desat.png",
         }
     ),
+    --tenorite
+    resource(
+        {
+          name = "tenorite",
+          order = "b",
+          map_color = {34,42,34},
+          mining_time = 1,
+          walking_sound = sounds.ore,
+          mining_visualisation_tint = {r = 34, g = 42, b = 34, a = 1.000}, --rgb(34, 42, 34)  --Color for ore 
+          --factoriopedia_simulation = simulations.factoriopedia_calcite,
+        },
+        {
+          probability_expression = 0
+        },
+        {
+          icon_subfolder = "ores_malcom/tenorite",
+          icon_image = "tenorite-1.png",
+          entity_subfolder = "procedural-ore",
+          entity_image_sheet = "copper-ore-desat.png",
+        }
+    ),
+    --pyrite
+    resource(
+      {
+        name = "pyrite",
+        order = "b",
+        map_color = {202,211,120},
+        mining_time = 1,
+        walking_sound = sounds.ore,
+        mining_visualisation_tint = {r = 202, g = 211, b = 120, a = 1.000}, --rgb(202, 211, 120)  --Color for ore 
+        --factoriopedia_simulation = simulations.factoriopedia_calcite,
+      },
+      {
+        probability_expression = 0
+      },
+      {
+        icon_subfolder = "ores_malcom/pyrite",
+        icon_image = "pyrite-1.png",
+        entity_subfolder = "procedural-ore",
+        entity_image_sheet = "iron-ore-desat.png",
+      }
+    ),
+    --siderite
+    resource(
+      {
+        name = "siderite",
+        order = "b",
+        map_color = {210,69,69},
+        mining_time = 1,
+        walking_sound = sounds.ore,
+        mining_visualisation_tint = {r = 210, g = 69, b = 69, a = 1.000}, --rgb(210, 69, 69)  --Color for ore 
+        --factoriopedia_simulation = simulations.factoriopedia_calcite,
+      },
+      {
+        probability_expression = 0
+      },
+      {
+        icon_subfolder = "ores_malcom/siderite",
+        icon_image = "siderite-1.png",
+        entity_subfolder = "procedural-ore",
+        entity_image_sheet = "iron-ore-desat.png",
+      }
+    ),
+    --hematite
+    resource(
+      {
+        name = "hematite",
+        order = "b",
+        map_color = {211,120,120},
+        mining_time = 1,
+        walking_sound = sounds.ore,
+        mining_visualisation_tint = {r = 211, g = 120, b = 120, a = 1.000}, --rgb(211, 120, 120)  --Color for ore 
+        --factoriopedia_simulation = simulations.factoriopedia_calcite,
+      },
+      {
+        probability_expression = 0
+      },
+      {
+        icon_subfolder = "ores_malcom/hematite",
+        icon_image = "hematite-1.png",
+        entity_subfolder = "procedural-ore",
+        entity_image_sheet = "iron-ore-desat.png",
+      }
+    ),
+
+    --ROUGH ORES
+
+    --covellite-rough
+    resource(
+        {
+          name = "covellite-rough",
+          order = "b",
+          map_color = {60,60,90},
+          mining_time = 1,
+          walking_sound = sounds.ore,
+          mining_visualisation_tint = {r = 60, g = 60, b = 90, a = 1.000}, --rgb(123, 123, 180)  --Color for ore 
+          --factoriopedia_simulation = simulations.factoriopedia_calcite,
+        },
+        {
+          probability_expression = 0
+        },
+        {
+          icon_subfolder = "ores_malcom/low-quality/covellite",
+          icon_image = "covellite-1.png",
+          entity_subfolder = "procedural-ore",
+          entity_image_sheet = "copper-ore-desat.png",
+        }
+    ),
+    --malachite-rough
+    resource(
+        {
+          name = "malachite-rough",
+          order = "b",
+          map_color = {17,100,45},
+          mining_time = 1,
+          walking_sound = sounds.ore,
+          mining_visualisation_tint = {r = 17, g = 100, b = 45, a = 1.000}, --rgb(34, 206, 89)  --Color for ore 
+          --factoriopedia_simulation = simulations.factoriopedia_calcite,
+        },
+        {
+          probability_expression = 0
+        },
+        {
+          icon_subfolder = "ores_malcom/low-quality/malachite",
+          icon_image = "malachite-1.png",
+          entity_subfolder = "procedural-ore",
+          entity_image_sheet = "copper-ore-desat.png",
+        }
+    ),
+    --tenorite-rough
+    resource(
+        {
+          name = "tenorite-rough",
+          order = "b",
+          map_color = {80,80,80},
+          mining_time = 1,
+          walking_sound = sounds.ore,
+          mining_visualisation_tint = {r = 80, g = 80, b = 80, a = 1.000}, --rgb(34, 42, 34)  --Color for ore 
+          --factoriopedia_simulation = simulations.factoriopedia_calcite,
+        },
+        {
+          probability_expression = 0
+        },
+        {
+          icon_subfolder = "ores_malcom//low-quality/tenorite",
+          icon_image = "tenorite-1.png",
+          entity_subfolder = "procedural-ore",
+          entity_image_sheet = "copper-ore-desat.png",
+        }
+    ),
+    --pyrite-rough
+    resource(
+      {
+        name = "pyrite-rough",
+        order = "b",
+        map_color = {100,105,60},
+        mining_time = 1,
+        walking_sound = sounds.ore,
+        mining_visualisation_tint = {r = 100, g = 105, b = 60, a = 1.000}, --rgb(202, 211, 120)  --Color for ore 
+        --factoriopedia_simulation = simulations.factoriopedia_calcite,
+      },
+      {
+        probability_expression = 0
+      },
+      {
+        icon_subfolder = "ores_malcom/low-quality/pyrite",
+        icon_image = "pyrite-1.png",
+        entity_subfolder = "procedural-ore",
+        entity_image_sheet = "iron-ore-desat.png",
+      }
+    ),
+    --siderite-rough
+    resource(
+      {
+        name = "siderite-rough",
+        order = "b",
+        map_color = {105,35,35},
+        mining_time = 1,
+        walking_sound = sounds.ore,
+        mining_visualisation_tint = {r = 105, g = 35, b = 35, a = 1.000}, --rgb(210, 69, 69)  --Color for ore 
+        --factoriopedia_simulation = simulations.factoriopedia_calcite,
+      },
+      {
+        probability_expression = 0
+      },
+      {
+        icon_subfolder = "ores_malcom/low-quality/siderite",
+        icon_image = "siderite-1.png",
+        entity_subfolder = "procedural-ore",
+        entity_image_sheet = "iron-ore-desat.png",
+      }
+    ),
+    --hematite-rough
+    resource(
+      {
+        name = "hematite-rough",
+        order = "b",
+        map_color = {100,60,60},
+        mining_time = 1,
+        walking_sound = sounds.ore,
+        mining_visualisation_tint = {r = 100, g = 60, b = 60, a = 1.000}, --rgb(211, 120, 120)  --Color for ore 
+        --factoriopedia_simulation = simulations.factoriopedia_calcite,
+      },
+      {
+        probability_expression = 0
+      },
+      {
+        icon_subfolder = "ores_malcom/low-quality/hematite",
+        icon_image = "hematite-1.png",
+        entity_subfolder = "procedural-ore",
+        entity_image_sheet = "iron-ore-desat.png",
+      }
+    ),
+
+    --Fluoride
+    resource(
+      {
+        name = "fluorite",
+        order = "b",
+        map_color = {120,211,144},
+        mining_time = 1,
+        walking_sound = sounds.ore,
+        mining_visualisation_tint = {r = 120, g = 211, b = 144, a = 1.000}, --rgb(120, 211, 144)  --Color for ore 
+        --factoriopedia_simulation = simulations.factoriopedia_calcite,
+      },
+      {
+        probability_expression = 0
+      },
+      {
+        icon_subfolder = "ores_malcom/fluorite",
+        icon_image = "fluorite-1.png",
+        entity_subfolder = "procedural-ore",
+        entity_image_sheet = "calcite-desat.png",
+      }
+    ),
+
+    --PROCEDURAL ORES
+    create_procedural_ore("alpha-ore-raw", {r = 200, g = 0,  b = 0, a = 1}), --rgb(200, 0, 0)
+    create_procedural_ore("beta-ore-raw", {r = 222,g = 138,  b = 20,  a = 1}), --rgb(222, 138, 20)
+    create_procedural_ore("gamma-ore-raw", {r = 232, g = 221, b = 12, a = 1}), --rgb(232, 221, 12)
+    create_procedural_ore("delta-ore-raw", {r = 0,  g = 200,b = 0, a = 1}), --rgb(0, 200, 0)
+    create_procedural_ore("epilson-ore-raw",{r = 37,  g = 18, b = 230, a = 1}), --rgb(37, 42, 230)
+    create_procedural_ore("omega-ore-raw", {r = 102,  g = 18, b = 212, a = 1}), --rgb(102, 18, 212)
+
 })
